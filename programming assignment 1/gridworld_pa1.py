3a15954e179ddfb232290ecdc9c717c06a9665bf
Navin <97350894+N47IN@users.noreply.github.com>
2024-02-20 16:39:12 +0530

    Update gridworld_pa1.py

--- programming assignment 1/gridworld_pa1.py
+++ programming assignment 1/gridworld_pa1.py
@@ -31,7 +31,7 @@ class GridWorld:
         The goal states for the gridworld where n is the number of goal
         states.
     """
-    def __init__(self, num_rows, num_cols, start_state, goal_states, wind = False, max_steps = 100):
+    def __init__(self, num_rows, num_cols, start_state, goal_states, wind = False):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.start_state = start_state
@@ -47,7 +47,45 @@ class GridWorld:
         self.gamma = 1 # default is no discounting
         self.wind = wind
 
-        self.max_steps = max_steps
+    def render_world(self):
+        matrix = np.zeros((10, 10)) 
+        matrix[self.obs_states[:, 0], self.obs_states[:, 1]] = 1
+        matrix[self.bad_states[:, 0], self.bad_states[:, 1]] = 2
+        matrix[self.restart_states[:, 0], self.restart_states[:, 1]] = 3
+        matrix[self.goal_states[:, 0], self.goal_states[:, 1]] = 4
+        matrix[self.start_state[:, 0], self.start_state[:, 1]] = 5
+        # Define colors for each element in the matri
+        plt.imshow(matrix, cmap='Pastel1', vmin=0, vmax=5)
+
+# Add grid lines
+        plt.grid(True, color='black', linewidth=2)
+
+
+# Add colorbar legend
+        cbar = plt.colorbar(ticks=[0, 1, 2, 3, 4, 5])
+        cbar.ax.set_yticklabels(['Empty', 'Obstruction', 'Bad State', 'Restart State', 'Goal State', 'Start State'])
+
+# Show the plot
+        plt.show()
+
+    def render_state(self,state):
+        matrix = np.zeros((10, 10))
+        state = seq_to_col_row(state,num_cols=10)
+        matrix[state[0][0],state[0][1]] = 6
+        matrix[self.obs_states[:, 0], self.obs_states[:, 1]] = 1
+        matrix[self.bad_states[:, 0], self.bad_states[:, 1]] = 2
+        matrix[self.restart_states[:, 0], self.restart_states[:, 1]] = 3
+        matrix[self.goal_states[:, 0], self.goal_states[:, 1]] = 4
+        matrix[self.start_state[:, 0], self.start_state[:, 1]] = 5
+        # Define colors for each element in the matri
+        plt.imshow(matrix, cmap='Pastel1', vmin=0, vmax=5)
+
+        plt.grid(True, color='black', linewidth=2)
+
+        cbar = plt.colorbar(ticks=[0, 1, 2, 3, 4, 5, 6])
+        cbar.ax.set_yticklabels(['Empty', 'Obstruction', 'Bad State', 'Restart State', 'Goal State', 'Start State','Curr State'])
+        plt.show()
+        
 
     def add_obstructions(self, obstructed_states=None, bad_states=None, restart_states=None):
 
@@ -186,17 +224,10 @@ class GridWorld:
         return next_state
 
     def reset(self):
-      self.steps = 0
       return int(self.start_state_seq)
 
     def step(self, state, action):
         
-        self.done = False
-        self.steps += 1
-        if self.steps >= self.max_steps:
-            self.done = True
-
-            
         p, r = 0, np.random.random()
         for next_state in range(self.num_states):
 
@@ -210,9 +241,9 @@ class GridWorld:
           arr = self.P[next_state, :, 3]
           next_next = np.where(arr == np.amax(arr))
           next_next = next_next[0][0]
-          return next_next, self.R[next_next], self.done
+          return next_next, self.R[next_next]
         else:
-          return next_state, self.R[next_state], self.done
+          return next_state, self.R[next_state]
         
 
 
